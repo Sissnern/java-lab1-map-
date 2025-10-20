2c40b0a4c37db9555f6c8702de1c06ebb02b07b4
Stoia Paul <vasile.stoia@stud.ubbcluj.ro>
2025-10-20 17:34:40 +0300

    ...

--- src/Universitaet.java
+++ src/Universitaet.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Universitaet {
+
+    //metoda ajutatoare care sa copieze array ul de note dat, pt a putea sa ramana nemodificat
+    public static int[] copyArray(int[] notenArray) {
+        int lenght = notenArray.length;
+
+
+    }
 }
