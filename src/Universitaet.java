f5ab579e6b6bc3bcb18fd0074a045b2ab051f90b
Stoia Paul <vasile.stoia@stud.ubbcluj.ro>
2025-10-20 17:44:39 +0300

    ok

--- src/Universitaet.java
+++ src/Universitaet.java
@@ -1,9 +1,23 @@
 public class Universitaet {
 
+//EX 1 __________________________________________________________________________________________
     //metoda ajutatoare care sa copieze array ul de note dat, pt a putea sa ramana nemodificat
     public static int[] copyArray(int[] notenArray) {
-        int lenght = notenArray.length;
 
+        int[] copiedArray = new int[notenArray.length];
+        for (int i = 0; i < notenArray.length; i++) {
+            copiedArray[i] = notenArray[i];
+        }
+        return copiedArray;
+    }
 
+    //Ex 1 - 1
+    public static int[] nichtAusreichenderNoten(int[] notenArray){
+        int[]
+        for(int i=0; i<notenArray.length; i++){
+            if(notenArray[i] % 5 < 3){
+
+            }
+        }
     }
 }
